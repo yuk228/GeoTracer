af3edeccf9d8e30ae23e176c605ee7f02f28bf41
Yuk <194586153+yuk228@users.noreply.github.com>
2025-10-15 01:45:10 +0900

    swrを追加

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,12 @@ import { Turnstile } from "next-turnstile";
 
 export default function Page() {
   return (
-    <main className="flex flex-col items-center justify-center max-w-4xl mx-auto">
+    <main className="flex flex-col items-center justify-center">
       <div className="flex items-center gap-2 justify-center mt-20">
         <Navigation size={48} />
         <h1 className="text-4xl font-bold">GeoTracer</h1>
       </div>
-      <div className="items-center justify-center mt-20 flex w-full max-w-md flex-col gap-2">
+      <div className="items-center justify-center mt-20 flex w-full max-w-md flex-col gap-2 px-4">
         <Input type="url" placeholder="Enter URL" aria-label="URL" />
         <Input
           type="url"
