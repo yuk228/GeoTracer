ee7a1e061aeb90245cd6261200cb98771e773ce6
Yuk <194586153+yuk228@users.noreply.github.com>
2025-10-14 23:18:10 +0900

    UIを作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { Navigation } from "lucide-react";
 
 export default function Page() {
   return (
-    <main>
+    <main className="flex flex-col items-center justify-center">
       <div className="flex items-center gap-2 justify-center mt-20">
         <Navigation size={48} />
         <h1 className="text-4xl font-bold">GeoTracer</h1>
+      </div>
+      <div className="items-center justify-center mt-20 flex flex-col gap-2">
+        <Input type="url" placeholder="Enter URL" />
+        <Button>Shorten</Button>
       </div>
     </main>
   );
